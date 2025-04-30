90c3acfaf37c9461f582f652534ff25fd842c5d0
Chris Kulhanek <98656265+chrikulhan@users.noreply.github.com>
2025-04-30 17:44:59 +0000

    try to add photo

--- flask_business_site/venv/bin/activate.fish
+++ flask_business_site/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/ExtraCreditBusinessWebsite/flask_business_site/venv"
+set -gx VIRTUAL_ENV "/workspaces/FamilyArchiveWebsite/flask_business_site/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
